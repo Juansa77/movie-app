8d026aaf124e1b288afd2e714c3fde3eb76b0e95
Juansa77 <dedalus1977@gmail.com>
2023-04-07 06:51:47 +0200

    Added react hook form  in login

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import { useContext, useRef } from 'react';
+import { useContext } from 'react';
+import { useForm } from 'react-hook-form';
 import { Navigate } from 'react-router-dom';
 
 import { UserContext } from '../../components/contexts/UserContext';
 
 const Home = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+  const onFormSubmit = (values) => {
+    login(values.username);
+  };
   //Nos traemos el login del userContext mediante destructuring
   const { login } = useContext(UserContext);
 
   //useNavigate nos proporciona un link  los elementos definidos en Routes
 
-  const textInput = useRef(null);
-
   return (
     <div className="loginContainer">
       {!localStorage.getItem('user') ? (
-        <>
+        <form onSubmit={handleSubmit(onFormSubmit)}>
           <h3>Por favor, introduzca su nombre de usuario</h3>
-          <input type="text" ref={textInput} placeholder="Nombre de usuario" />
+          <input
+            type="text"
+            {...register('username', {
+              required: true, // Si no hay valor, no activará la función del login
+              minLength: 2, // Si no tiene al menos dos letras, no activa la función
+            })}
+            placeholder="Nombre de usuario"
+          />
+          {/* Mostramos el error si no hay username ya que es requerido */}
+          {errors.username ? (
+            <p className="error">Campo obligatorio, debe tener al menos dos caracteres</p>
+          ) : null}
           {/*Al pulsar en el botón, llama a la funcuión user para que meta en localStorage el valor del input*/}
-          <button onClick={() => login(textInput.current.value)}>Ingresar</button>{' '}
-        </>
+          <button type="submit">Ingresar</button>{' '}
+        </form>
       ) : (
         <>
           <Navigate replace to="/main" />
